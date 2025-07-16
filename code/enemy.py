826aba967d5eaf2b31a80de446fb656db36f8206
samiul alim <Shamiul-alim>
2025-07-17 00:17:40 +0600

    final

--- code/enemy.py
+++ code/enemy.py
@@ -4,7 +4,7 @@ from entity import Entity
 from support import *
 
 class Enemy(Entity):
-	def __init__(self,monster_name,pos,groups,obstacle_sprites):
+	def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles,add_exp):
 
 		# general setup
 		super().__init__(groups)
@@ -36,6 +36,22 @@ class Enemy(Entity):
 		self.can_attack = True
 		self.attack_time = None
 		self.attack_cooldown = 400
+		self.damage_player = damage_player
+		self.trigger_death_particles = trigger_death_particles
+		self.add_exp = add_exp
+
+		# invincibility timer
+		self.vulnerable = True
+		self.hit_time = None
+		self.invincibility_duration = 300
+
+		# sounds
+		self.death_sound = pygame.mixer.Sound('audio/death.wav')
+		self.hit_sound = pygame.mixer.Sound('audio/hit.wav')
+		self.attack_sound = pygame.mixer.Sound(monster_info['attack_sound'])
+		self.death_sound.set_volume(0.6)
+		self.hit_sound.set_volume(0.6)
+		self.attack_sound.set_volume(0.6)
 
 	def import_graphics(self,name):
 		self.animations = {'idle':[],'move':[],'attack':[]}
@@ -70,7 +86,8 @@ class Enemy(Entity):
 	def actions(self,player):
 		if self.status == 'attack':
 			self.attack_time = pygame.time.get_ticks()
-			print('attack')
+			self.damage_player(self.attack_damage,self.attack_type)
+			self.attack_sound.play()
 		elif self.status == 'move':
 			self.direction = self.get_player_distance_direction(player)[1]
 		else:
@@ -88,16 +105,50 @@ class Enemy(Entity):
 		self.image = animation[int(self.frame_index)]
 		self.rect = self.image.get_rect(center = self.hitbox.center)
 
-	def cooldown(self):
+		if not self.vulnerable:
+			alpha = self.wave_value()
+			self.image.set_alpha(alpha)
+		else:
+			self.image.set_alpha(255)
+
+	def cooldowns(self):
+		current_time = pygame.time.get_ticks()
 		if not self.can_attack:
-			current_time = pygame.time.get_ticks()
 			if current_time - self.attack_time >= self.attack_cooldown:
 				self.can_attack = True
 
+		if not self.vulnerable:
+			if current_time - self.hit_time >= self.invincibility_duration:
+				self.vulnerable = True
+
+	def get_damage(self,player,attack_type):
+		if self.vulnerable:
+			self.hit_sound.play()
+			self.direction = self.get_player_distance_direction(player)[1]
+			if attack_type == 'weapon':
+				self.health -= player.get_full_weapon_damage()
+			else:
+				self.health -= player.get_full_magic_damage()
+			self.hit_time = pygame.time.get_ticks()
+			self.vulnerable = False
+
+	def check_death(self):
+		if self.health <= 0:
+			self.kill()
+			self.trigger_death_particles(self.rect.center,self.monster_name)
+			self.add_exp(self.exp)
+			self.death_sound.play()
+
+	def hit_reaction(self):
+		if not self.vulnerable:
+			self.direction *= -self.resistance
+
 	def update(self):
+		self.hit_reaction()
 		self.move(self.speed)
 		self.animate()
-		self.cooldown()
+		self.cooldowns()
+		self.check_death()
 
 	def enemy_update(self,player):
 		self.get_status(player)
